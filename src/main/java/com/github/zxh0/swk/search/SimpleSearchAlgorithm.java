f39380c96d321f32e9d6506e1633df5d8e2671dd
zxh <zhangxiuhong2013@gmail.com>
2013-10-17 13:09:15 +0800

    add SimpleSearchAlgorithm

---
+++ src/main/java/com/github/zxh0/swk/search/SimpleSearchAlgorithm.java
@@ -0,0 +1,26 @@
+package com.github.zxh0.swk.search;
+
+import com.github.zxh0.swk.SensitiveWordSearchAlgorithm;
+import java.util.List;
+
+public class SimpleSearchAlgorithm implements SensitiveWordSearchAlgorithm {
+
+    private List<String> sensitiveWords;
+    
+    @Override
+    public void init(List<String> sensitiveWords) {
+        this.sensitiveWords = sensitiveWords;
+    }
+
+    @Override
+    public String search(String text, int startIndex) {
+        for (String word : sensitiveWords) {
+            if (text.indexOf(word, startIndex) > -1) {
+                return word;
+            }
+        }
+        
+        return null;
+    }
+    
+}
