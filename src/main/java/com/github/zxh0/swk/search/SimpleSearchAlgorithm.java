a0b8a7339a6123b2d5582228cdacb67d6e591d20
zxh <zhangxiuhong2013@gmail.com>
2013-10-17 14:48:52 +0800

    refactor

--- src/main/java/com/github/zxh0/swk/search/SimpleSearchAlgorithm.java
+++ src/main/java/com/github/zxh0/swk/search/SimpleSearchAlgorithm.java
@@ -2,6 +2,8 @@ package com.github.zxh0.swk.search;
 
 import com.github.zxh0.swk.SensitiveWordSearchAlgorithm;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SimpleSearchAlgorithm implements SensitiveWordSearchAlgorithm {
@@ -11,6 +13,18 @@ public class SimpleSearchAlgorithm implements SensitiveWordSearchAlgorithm {
     @Override
     public void init(List<String> sensitiveWords) {
         this.sensitiveWords = new ArrayList<>(sensitiveWords);
+        sortSensitiveWordsByLength();
+    }
+    
+    private void sortSensitiveWordsByLength() {
+        Collections.sort(sensitiveWords, new Comparator<String>() {
+
+            @Override
+            public int compare(String s1, String s2) {
+                return Integer.compare(s2.length(), s1.length());
+            }
+            
+        });
     }
 
     @Override
---
+++ src/test/java/com/github/zxh0/swk/SensitiveWordSearchAlgorithmTest.java
@@ -0,0 +1,25 @@
+package com.github.zxh0.swk;
+
+import com.github.zxh0.swk.search.SimpleSearchAlgorithm;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SensitiveWordSearchAlgorithmTest {
+    
+    private List<String> sensitiveWords = Arrays.asList("妈蛋", "他妈的", "妈的", "他妈的巴子的");
+    
+    @Test
+    public void simpleSearchAlgorithm() {
+        SensitiveWordSearchAlgorithm algorithm = new SimpleSearchAlgorithm();
+        algorithm.init(sensitiveWords);
+        
+        Assert.assertEquals("他妈的巴子的", algorithm.search("他妈的巴子的", 0));
+        Assert.assertEquals("他妈的巴子的", algorithm.search("去他妈的巴子的！", 0));
+        Assert.assertEquals("他妈的", algorithm.search("他妈的", 0));
+        Assert.assertEquals("他妈的", algorithm.search("去他妈的", 0));
+        Assert.assertEquals("妈的", algorithm.search("妈的", 0));
+    }
+    
+}
