4a949de1fd611994ba21639d8fd9483a118f58fc
zxh <zhangxiuhong2013@gmail.com>
2013-10-17 12:41:48 +0800

    create project

---
+++ src/main/java/com/github/zxh0/swk/SensitiveWordKiller.java
@@ -0,0 +1,19 @@
+package com.github.zxh0.swk;
+
+import java.util.List;
+
+public class SensitiveWordKiller {
+    
+    public boolean check(String str) {
+        return false;
+    }
+    
+    public List<String> search(String str) {
+        return null;
+    }
+    
+    public String replace(String str) {
+        return str;
+    }
+    
+}
