d33b93d943db0745d8b004a000d5d7c979839057
zxh <zhangxiuhong2013@gmail.com>
2013-10-18 09:28:17 +0800

    refactor unit test

--- src/test/java/com/github/zxh0/swk/SensitiveWordSearchAlgorithmTest.java
+++ src/test/java/com/github/zxh0/swk/SensitiveWordSearchAlgorithmTest.java
@@ -9,32 +9,27 @@ import org.junit.Test;
 
 public class SensitiveWordSearchAlgorithmTest {
     
-    private List<String> sensitiveWords = Arrays.asList("妈蛋", "他妈的", "妈的", "他妈的巴子的");
+    private List<String> sensitiveWords = Arrays.asList(
+            "草", "草泥", "草泥马",
+            "干", "干妈", "老干妈",
+            "妈", "妈蛋", "妈蛋黄派");
     
     @Test
-    public void simpleSearchAlgorithm() {
-        SensitiveWordSearchAlgorithm algorithm = new SimpleSearchAlgorithm();
-        algorithm.init(sensitiveWords);
+    public void oneWord() {
+        SensitiveWordSearchAlgorithm simple = new SimpleSearchAlgorithm();
+        simple.init(sensitiveWords);
+        oneWord(simple);
         
-        Assert.assertEquals("他妈的巴子的", algorithm.search("他妈的巴子的", 0).getWord());
-        Assert.assertEquals("他妈的巴子的", algorithm.search("去他妈的巴子的！", 0).getWord());
-        Assert.assertEquals("他妈的", algorithm.search("他妈的", 0).getWord());
-        Assert.assertEquals("他妈的", algorithm.search("去他妈的", 0).getWord());
-        Assert.assertEquals("妈的", algorithm.search("妈的", 0).getWord());
-        Assert.assertEquals(null, algorithm.search("大姨妈", 0));
+        SensitiveWordSearchAlgorithm binary = new BinarySearchAlgorithm();
+        binary.init(sensitiveWords);
+        oneWord(binary);
     }
     
-    @Test
-    public void binarySearchAlgorithm() {
-        SensitiveWordSearchAlgorithm algorithm = new BinarySearchAlgorithm();
-        algorithm.init(sensitiveWords);
-        
-        Assert.assertEquals("他妈的巴子的", algorithm.search("他妈的巴子的", 0).getWord());
-        Assert.assertEquals("他妈的巴子的", algorithm.search("去他妈的巴子的！", 0).getWord());
-        Assert.assertEquals("他妈的", algorithm.search("他妈的", 0).getWord());
-        Assert.assertEquals("他妈的", algorithm.search("去他妈的", 0).getWord());
-        Assert.assertEquals("妈的", algorithm.search("妈的", 0).getWord());
-        Assert.assertEquals(null, algorithm.search("大姨妈", 0));
+    private void oneWord(SensitiveWordSearchAlgorithm algorithm) {
+        Assert.assertEquals("草", algorithm.search("草", 0).getWord());
+        Assert.assertEquals("草", algorithm.search("青草", 0).getWord());
+        Assert.assertEquals("草", algorithm.search("草地", 0).getWord());
     }
+    
     
 }
